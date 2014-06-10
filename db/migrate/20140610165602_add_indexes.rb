c112cc230d24103e81071e01448d3e7ecdf48851
Carter <Carter@Chaouki-Abdallahs-MacBook-Pro-7.local>
2014-06-10 11:49:45 -0600

    T&P app with file uploads

--- db/migrate/20140610165602_add_indexes.rb
+++ db/migrate/20140610165602_add_indexes.rb
@@ -1,22 +1,4 @@
 class AddIndexes < ActiveRecord::Migration
   def change
-  	add_index :committee_evaluations,  :review_id
-  	add_index :committee_evaluations, :committee_id
-  	add_index :committee_evaluations, :review_candidate_profile_id
-  	add_index :candidate_profiles, :user_id
-  	add_index :candidate_profiles, :User_department_id
-  	add_index :committees, :department_id
-  	add_index :committees, :department_college_id
-  	add_index :departments, :College_id
-  	add_index :reviews, :candidate_profile_id
-  	add_index :reviews, :committee_id
-  	add_index :reviews, :committee_department_id
-  	add_index :reviews, :committee_department_college_id
-  	add_index :users, :department_id
-  	add_index :users, :committee_id
-  	add_index :users, :department_college_id
-  	add_index :votes, :review_id
-  	add_index :votes, :candidate_profile_id
-  	add_index :votes, :user_id
   end
 end
