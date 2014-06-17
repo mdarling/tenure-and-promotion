af7e2c21bf4e5390d585d57a14b2b3fdc3251e2e
Tyler <tyster505@gmail.com>
2014-06-17 12:44:37 -0600

    User -> Candidate Profile + Show function (buttons don't work)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ActiveRecord::Base
-	validates_presence_of :acces_level
-	validates_presence_of :Committee_id
-	validates_presence_of :Department_College_id
-	validates_presence_of :Department_id 
-    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => "Must Be Valid"
-    validates_format_of :last_name, :with => /\A([-a-zA-Z])+\z/i, :on => :create, :message => "Must Be Valid"
-    validates_format_of :first_name, :with => /\A([-a-zA-Z])+\z/i, :on => :create, :message => "Must Be Valid"
+    User
+  after_create :build_profile
+
+  def build_profile
+    CandidateProfile.create(user: self) 
+  end
+
+      has_one :candidate_profile, dependent: :destroy
+  def full_name
+      "#{self.first_name} #{self.last_name}" 
+    end
+
+	#validates_presence_of :acces_level
+	#validates_presence_of :Committee_id
+	#validates_presence_of :Department_College_id
+	#validates_presence_of :Department_id 
+    #validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => "Must Be Valid"
+    #validates_format_of :last_name, :with => /\A([-a-zA-Z])+\z/i, :on => :create, :message => "Must Be Valid"
+    #validates_format_of :first_name, :with => /\A([-a-zA-Z])+\z/i, :on => :create, :message => "Must Be Valid"
+  
 end
 
