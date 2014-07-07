c028166c1bc58d0f92dffe6fdbc51054f08ddc16
Carter <Carter@Chaouki-Abdallahs-MacBook-Pro-7.local>
2014-07-07 13:27:31 -0600

    Fixed Compiling and Publish Pages in the wizard

--- app/controllers/initial_controller.rb
+++ app/controllers/initial_controller.rb
@@ -2,26 +2,34 @@ class InitialController < ApplicationController
   layout "wizard"
   def index
   @pagename="Test File"
+  @pagetype=1 
   end
   def cv
   @pagename="Curriculum Vitae"
+  @pagetype=1
   end
   def ts
   @pagename="Teaching Statement"
+  @pagetype=1
   end
   def rs
   @pagename="Research Statement"
+  @pagetype=1
   end
   def ss
   @pagename="Service Statement"
+  @pagetype=1
   end
   def am
   @pagename="Additional Materials"
+  @pagetype=1
   end
   def publish
   @pagename="Publish"
+  @pagetype=2
   end
   def compile
   @pagename="Compiling"
+  @pagetype=2
   end
 end
