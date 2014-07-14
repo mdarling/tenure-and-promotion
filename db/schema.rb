e9c78102fe33a3220d34fe813d425e8134d1bf54
Tyler <tyster505@gmail.com>
2014-07-14 15:03:14 -0600

    Dropdown Just for Carol

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711165319) do
+ActiveRecord::Schema.define(version: 20140709192727) do
 
   create_table "candidate_profiles", force: true do |t|
     t.text     "Curriculum_Vitae"
@@ -109,11 +109,6 @@ ActiveRecord::Schema.define(version: 20140711165319) do
   end
 
   create_table "sign_ins", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "uploads", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
