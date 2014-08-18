9fe7892a0ceb74c1742c69cc341e721a3b418b84
rickpr <fdisk87@yahoo.com>
2014-08-18 04:35:15 -0600

    Next prawn attempt

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -67,11 +67,11 @@ class ConvertsController < ApplicationController
       @converts = @user.Converts.all
       #pdf_file_paths = ["1.pdf", "2.pdf"]
       Prawn::Document.generate("result.pdf", {:page_size => 'A4', :skip_page_creation => true}) do |pdf|
-        @converts.download.each do |pdf_file|
+        @converts.each do |pdf_file|
           #if File.exists?("./public#{pdf_file}")
-            pdf_temp_nb_pages = Prawn::Document.new(:template => "./public#{pdf_file}").page_count
+            pdf_temp_nb_pages = Prawn::Document.new(:template => "./public#{pdf_file.download}").page_count
             (1..pdf_temp_nb_pages).each do |i|
-            pdf.start_new_page(:template => "./public#{pdf_file}", :template_page => i)
+            pdf.start_new_page(:template => "./public#{pdf_file.download}", :template_page => i)
           #end
         end
       end
