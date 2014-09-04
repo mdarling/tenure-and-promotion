1db185de40ea2828dff6215b17798bfbe49c456c
Ricardo Piro-Rael <fdisk@Gregorys-iMac.local>
2014-09-03 23:21:57 -0600

    It's working now

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ActiveRecord::Base
 belongs_to :user
-has_many :Uploads, dependent: :destroy
+#has_many :Uploads, dependent: :destroy
   has_attached_file :upload
   do_not_validate_attachment_file_type :upload
 
@@ -11,7 +11,7 @@ has_many :Uploads, dependent: :destroy
       "size" => read_attribute(:upload_file_size),
       "url" => upload.url(:original),
 #      "delete_url" => upload_path(user_id,self),
-      "delete_url" => uploads_path,
+      "delete_url" => upload_path(self),
       "delete_type" => "DELETE"
     }
   end
